da0834ffde98090fcc75971e8dd4e14a93e139e3
zjria <zakaria.com.bd@gmail.com>
2024-02-15 19:45:29 +0600

    newsroom add

--- js/app.js
+++ js/app.js
@@ -1,41 +1,38 @@
-$(document).ready(function () {
-  $(".images-slide").slick({
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
+$(".testimonial-slider").slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  slickGoTo: 0,
+  speed: 3000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ],
-  });
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
-
-
-console.log('zakaria');
