4c2c64e7dd6aec7accaa7c24662204c050ba3ba1
zjria <zakaria.com.bd@gmail.com>
2024-02-24 12:15:29 +0600

    animation add

--- js/app.js
+++ js/app.js
@@ -34,8 +34,54 @@ $(".testimonial-slider").slick({
   ],
 });
 
-// navbar js
+// video animation
+var modal = document.getElementById("videoPopup");
+var btn = document.getElementById("openBtn");
+var span = document.getElementById("closeBtn");
+var video = document.getElementById("videoPlayer");
 
-document.querySelector(
-  "#sticky-header > div > div > div.col-lg-9 > nav > div.cleeny-btn > a"
-);
+btn.onclick = function () {
+  modal.style.display = "block";
+  // video.play();
+};
+span.onclick = function () {
+  modal.style.display = "none";
+  video.pause();
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    video.pause();
+  }
+};
+
+// video two
+var modalTwo = document.getElementById("videoPopupTwo");
+var btnTwo = document.getElementById("openBtnTwo");
+var spanTwo = document.getElementById("closeBtnTwo");
+var videoTwo = document.getElementById("videoPlayerTwo");
+
+btnTwo.onclick = function () {
+  modalTwo.style.display = "block";
+  // video.play();
+};
+spanTwo.onclick = function () {
+  modalTwo.style.display = "none";
+  videoTwo.pause();
+};
+window.onclick = function (event) {
+  if (event.target == modalTwo) {
+    modalTwo.style.display = "none";
+    videoTwo.pause();
+  }
+};
+
+// =================Our Dedicated Investigators===========================
+var shareButtons = document.querySelectorAll(".share-button");
+var socialLinksList = document.querySelectorAll(".social-links");
+
+shareButtons.forEach(function (shareButton, index) {
+  shareButton.addEventListener("click", function () {
+    socialLinksList[index].classList.toggle("show");
+  });
+});
