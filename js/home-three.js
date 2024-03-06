5995bf9328fffdc353eee6dd82ce32d05374f62a
zjria <zakaria.com.bd@gmail.com>
2024-03-06 18:15:52 +0600

    home three some section add

--- js/home-three.js
+++ js/home-three.js
@@ -77,29 +77,30 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// video animation
-var modal = document.getElementById("videoPopup");
-var btn = document.getElementById("openBtn");
-var span = document.getElementById("closeBtn");
-var video = document.getElementById("videoPlayer");
+// Number Counting
 
-btn.onclick = function () {
-  modal.style.display = "block";
-  // video.play();
-};
-span.onclick = function () {
-  modal.style.display = "none";
-  video.pause();
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    video.pause();
+const countElement = document.querySelector("#num");
+let countNumber = 0;
+const upDateCounter = setInterval(function () {
+  countNumber++;
+  countElement.textContent = countNumber;
+
+  if (countNumber >= 79) {
+    clearInterval(upDateCounter);
   }
-};
+}, 50);
+
+// =================Our Dedicated Investigators===========================
+var shareButtons = document.querySelectorAll(".share-button");
+var socialLinksList = document.querySelectorAll(".social-links");
+
+shareButtons.forEach(function (shareButton, index) {
+  shareButton.addEventListener("click", function () {
+    socialLinksList[index].classList.toggle("hideLink");
+  });
+});
 
 // video two
-
 var modalTwo = document.getElementById("videoPopupTwo");
 var btnTwo = document.getElementById("openBtnTwo");
 var spanTwo = document.getElementById("closeBtnTwo");
@@ -107,7 +108,7 @@ var videoTwo = document.getElementById("videoPlayerTwo");
 
 btnTwo.onclick = function () {
   modalTwo.style.display = "block";
-  // video.play();
+  video.play();
 };
 spanTwo.onclick = function () {
   modalTwo.style.display = "none";
